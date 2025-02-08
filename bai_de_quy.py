56bd926067fc7278655e7335e1fdb97f2ba28df3
duynippon <len.mang.7@gmail.com>
2025-02-08 15:56:15 +0700

    chinh sua bai de quy

--- bai_de_quy.py
+++ bai_de_quy.py
@@ -1,1 +1,27 @@
-print("hello")
+from pprint import pprint
+
+
+def list_dict(start, target):
+    lst = []
+
+    if start == target:
+
+        return []
+    else:
+        lst.append({"id": start, "value": list_dict(start + 1, target)})
+
+    return lst
+
+
+def after(start, target):
+    lst = list_dict(start, target)
+    lst.append({"id": target, "value": []})
+    return lst
+
+
+start = 1
+target = 5
+demo = after(start, target)
+
+
+pprint(demo)
